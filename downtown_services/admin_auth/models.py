d41e93976431fa46324095a0ec7391c48da372cb
Elbin12 <elbinshiby14@gmail.com>
2024-12-12 10:52:49 +0530

    notification added for both chat and others

--- downtown_services/admin_auth/models.py
+++ downtown_services/admin_auth/models.py
@@ -14,4 +14,31 @@ class SubCategories(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Subscription(models.Model):
+    tier_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.tier_name
+
+
+class SubscriptionFeatures(models.Model):
+    ANALYTICS_CHOICES = [
+        ('no_analytics', 'No Analytics'),
+        ('basic', 'Baisc'),
+        ('advanced', 'Advanced'),
+    ]
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, related_name='subscription_features')
+    platform_fee_perc = models.IntegerField()
+    service_add_count = models.CharField(max_length=20, default='0')
+    service_update_count = models.CharField(max_length=20, default='0')
+    user_requests_count = models.CharField(max_length=20, default='0')
+    analytics = models.CharField(max_length=20, choices=ANALYTICS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.subscription.tier_name} Features"
